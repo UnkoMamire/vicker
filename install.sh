f563f4ad8581ee248f735b131ba852cf9344fdd6
darnisee <kichikichi.9016@google.com>
2018-11-04 20:42:50 +0900

    first commit

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+ListFile=${ListFile:-`dirname $0`/list}
+PackName=${PackName:-mypack}
+
+while read line
+do
+	repo=`echo $line | cut -d: -f1`
+	type=`echo $line | cut -d: -f2`
+	build=`echo $line | cut -d: -f3`
+
+	installpath=~/.vim/pack/$PackName/$type
+	reponame=`basename $repo`
+	mkdir -p $installpath
+	if [ -d $installpath/$reponame ]; then
+		git -C $installpath/$reponame pull
+	else
+		git -C $installpath clone https://github.com/$repo
+	fi
+
+	echo "cd $installpath/$reponame; $build" | sh
+
+done < $ListFile
+
+if which nvim; then
+	which pip3 && ! (pip3 list --format=legacy | grep "neovim") && pip3 --user install neovim
+	nvim +'UpdateRemotePlugins | q'
+fi
